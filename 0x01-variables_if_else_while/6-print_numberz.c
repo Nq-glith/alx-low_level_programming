e4630fbce0513ba35811850034e0b217194122d3
Nq-glith <nxobile.mahlangu@gmail.com>
2023-04-24 15:54:05 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n <= 9; n++;)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
