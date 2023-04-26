0d2f6ed1d30039ea530a6969f532c346c1840e62
Nq-glith <nxobile.mahlangu@gmail.com>
2023-04-26 08:33:43 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of
+ * a number
+ *
+ * @i: input number as an integer
+ * Return: 0
+ */
+int print_last_digit(int i)
+{
+	int n;
+
+	n = i % 10;
+	if (n < 0)
+	{
+		_putchar(-n + 48);
+		return (-n);
+	}
+	else
+	{
+		_putchar(n + 48);
+		return (n);
+	}
+}
