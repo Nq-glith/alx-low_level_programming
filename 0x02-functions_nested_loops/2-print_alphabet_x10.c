2aa156efe0e0cda5715b420b6315f96dc608e1bd
Nq-glith <nxobile.mahlangu@gmail.com>
2023-04-26 05:32:08 -0700

    print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	int m;
+
+	for (n = 1; n <= 10; n++)
+	{
+		for (m = 97; m <= 122; m++)
+		{
+			_putchar(m);
+		}
+		_putchar('\n');
+	}
+}
