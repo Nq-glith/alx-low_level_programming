dbfce7574c475577d94fda3fea73663b69385ce3
Nq-glith <nxobile.mahlangu@gmail.com>
2023-04-26 15:45:47 -0700

    print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,44 +4,43 @@
  * print_times_table - print n times table
  * function that prints the n times table
  * starting with 0
- * @n: starting point
+ * @n: number of times table is to be printed
  * Return: Always 0
  */
 void print_times_table(int n)
 {
-	int x, y, z;
+	int a, b, op;
 
-	if (n >= 0 && n <= 15)
+	if (n <= 0 && n >= 15)
+		return;
 	{
-		for (x = 0; x <= n; x++)
+		for (a = 0; a <= n; a++)
 		{
-			for (y = 0; y <= n; y++)
+			_putchar(48);
+
+			for (b = 1; b <= n; b++)
 			{
-				z = y * x;
-				if (y == 0)
+				op = a * b;
+				_putchar(44);
+				_putchar(32);
+			
+				if (op <= 9)
 				{
-					_putchar(z + '0');
-				} else if (z < 10 && y != 0)
+					_putchar(32);
+					_putchar(32);
+					_putchar(op + 48);
+				}
+				else if (op <= 99)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(z + '0');
-				} else if (z >= 10 && z < 100)
+					_putchar(32);
+					_putchar((op / 10) + 48);
+					_putchar((op % 10) + 48);
+				}
+				else
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((z / 10) + '0');
-					_putchar((z % 10) + '0');
-				} else if (z >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
+					_putchar(((op / 100) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
+					_putchar((op % 10) + 48);
 				}
 			}
 			_putchar('\n');
