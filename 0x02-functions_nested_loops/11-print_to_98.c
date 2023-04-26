d04c23f0ac38db808cf1703490befb73a72c7c7d
Nq-glith <nxobile.mahlangu@gmail.com>
2023-04-26 14:16:29 -0700

    print natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,18 +13,11 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (n = n; n <= 98; n++)
-		{
+		for (n = n; n <= 97; n++)
 			printf("%d, ", n);
-		}
 	}
-	else if (n >= 98)
-	{
-		for (n = n; n >= 98; n--)
-		{
+	else
+		for (n = n; n > 98; n--)
 			printf("%d, ", n);
-		}
-	}
-	printf("%d\n", n);
+	printf("98\n");
 }
-
