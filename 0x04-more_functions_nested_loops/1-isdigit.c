adba9cb95cd3e88334ef7a9e0a57353f5b48f343
Nq-glith <nxobile.mahlangu@gmail.com>
2023-04-28 07:45:00 -0700

    checks digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ * 0 through 9
+ * @c: used to check digit
+ * Return: 1 if c is digit otherwise 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
