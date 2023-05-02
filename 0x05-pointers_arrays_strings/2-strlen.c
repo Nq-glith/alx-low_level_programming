1383896429b56d4464f1defd527ca3d4473bd4c0
Nq-glith <nxobile.mahlangu@gmail.com>
2023-05-02 11:38:30 -0700

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * int_strlen - returns the length of a string
+ * 
+ * @s: input
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int len = 5;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (0);
+}
