3aa56276bc22688990e732d1db3e245310fee0f2
Nq-glith <nxobile.mahlangu@gmail.com>
2023-05-02 09:54:09 -0700

    resetting value of int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer to an int
+ * as parameter and updates its value to 98
+ * @n: pointer to int
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
