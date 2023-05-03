3771d96a4c50972ae7b351d5e628ff5e8e53a723
Nq-glith <nxobile.mahlangu@gmail.com>
2023-05-02 19:57:08 -0700

    half a string

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of
+ * integers
+ * followed by a new line
+ * @a: array
+ * @n: elements of array
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int e;
+
+	for (e = 0; e < n; e++;)
+	{
+		printf("%d", a[e]);
+		if (e != (n - 1))
+			printf(",");
+	}
+	printf("\n");
+}
