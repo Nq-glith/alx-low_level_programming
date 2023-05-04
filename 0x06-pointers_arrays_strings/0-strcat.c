bad95a551030dd1666e273e06a8dbd1d8b593179
Nq-glith <nxobile.mahlangu@gmail.com>
2023-05-04 07:57:21 -0700

    concatinating two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,18 +12,25 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	/*initialize pointer a, a will point to start of dest string*/
 	char *a = dest;
 
+	/*this loop will through a until it reaches the end of the dest string*/
 	while (*a != '\0')
 	{
 		a++;
 	}
+	/*once the loop reaches the end of the dest string*/
+	/*this loop will repeat through the source string*/
+	/*and append each charecter to the end of dest*/
 	while (*src != '\0')
 	{
 		*a = *src;
 		a++;
 		src++;
 	}
+	/*add a new terminating null byte to the end of the combined string*/
+	/*return pointer to the resulting string*/
 	*a = '\0';
 	return (dest);
 }
