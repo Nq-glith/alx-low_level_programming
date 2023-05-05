febd4362976b95b29c63c4f094366792fb3ade8a
Nq-glith <nxobile.mahlangu@gmail.com>
2023-05-05 16:54:43 -0700

    print buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_buffer - print a buffer
+ *
+ * @b: pointer
+ * @size: bytes of the buffer
+ * Return: string with all letters
+ */
+void print_buffer(char *b, int size)
+{
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	int i, j;
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				printf("%02x ", (unsigned char)b[i + j]);
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf(" ");
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				char c = isprint(b[i + j] ? b[i + j] : '.';
+
+				printf("%c", c);
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
