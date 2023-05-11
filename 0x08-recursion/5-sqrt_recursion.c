fc71c61e6bc66c9d7845ff4a49f8a601dfeaab23
Nq-glith <nxobile.mahlangu@gmail.com>
2023-05-10 20:47:01 -0700

    square root recrsion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,10 +2,9 @@
 
 int natural_sqrt_recursion(int n, int a);
 /**
- * natural_sqrt_recursion - recursive function that
+ * _sqrt_recursion - recursive function that
  * finds the square root of a number
- * @n: integer forwhich the square root is to be found
- * @a: iterator
+ * @n: integer for which the square root is to be found
  * Return: square root of n if found
  * -1 if not a perfect square
  */
@@ -15,7 +14,7 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	return (natural_sqrt_recursion (n, 0));
+	return (natural_sqrt_recursion(n, 0));
 }
 
 /**
