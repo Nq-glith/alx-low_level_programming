a7f58b8784fea0732d21d3fe4db0e6ff35f154ac
Nq-glith <nxobile.mahlangu@gmail.com>
2023-05-16 19:40:05 -0700

    strtow function

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * *argstostr - concatenates all the arguments
+ * of the program
+ * @ac: int type
+ * @av: Char type pointer
+ * Return: pointer to a new string else NULL if it fails
+ * NULL f ac == 0 or av == NULL
+ */
+char *argstostr(int ac, char **av)
+{
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * **strtow - splits a string into words
+ * The function returns a pointer to an array of strings (words)
+ * Each element of this array should contain a single word,
+ * null-terminated
+ * The last element of the returned array should be NULL
+ * Words are separated by spaces
+ * @str: string
+ * Return: pointer to string on success
+ * NULL if str == NULL or str == ""
+ * if fucntion fails return NULL
+ */
+char **strtow(char *str)
+{
+}
