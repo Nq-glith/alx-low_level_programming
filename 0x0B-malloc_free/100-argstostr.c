63d8ae0c15ebf4560f5e6db17abb135c2b7388ae
Nq-glith <nxobile.mahlangu@gmail.com>
2023-05-17 17:57:54 -0700

    concatenating arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,4 +12,38 @@
  */
 char *argstostr(int ac, char **av)
 {
+	int i, j, k, l = 0;
+	char *ptr;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < ac; j++)
+	{
+		for (k = 0; av[j][k] != '\0'; k++)
+		{
+			i += 1;
+		}
+		i += 1;
+	}
+	i += 1;
+	ptr = malloc(sizeof(char) * i);
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (j = 0; j < ac; j++)
+	{
+		for (k = 0; av[j][k] != '\0'; k++)
+		{
+			ptr[l] = av[j][k];
+			l++;
+		}
+		ptr[l] = '\n';
+		l++;
+	}
+	ptr[l] = '\0';
+	return (ptr);
 }
