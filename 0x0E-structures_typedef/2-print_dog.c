fd72f44d8b7b937fb780d3eb4b98146fa8b6250c
Nq-glith <nxobile.mahlangu@gmail.com>
2023-05-23 07:44:56 -0700

    header file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,4 @@
+#include "dog.h"
+#include <stdlib.h>
+
+void print_dog(struct dog *d)
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,18 @@
 #ifndef dog_h
 #define dog_h
 
+/**
+ * struct dog - new type structure
+ * @name: first member
+ * @age: second member
+ * @owner: third memeber
+ * the header file defines a structure named dog, with three memebrs
+ */
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
